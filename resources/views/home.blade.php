3ac63ee5101fa3455d5c425b806562833d5ab758
zalmi <78726819+zalmicoding@users.noreply.github.com>
2022-03-08 00:53:12 -0800

    updated by hk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
- {{-- helllllllllllllllllllllllllllllllllllllo --}}
+{{---sdajskdjaskjdkjaskjdkajshdkjhaskjhdkjashdkhaksjhdkajshdkjashkdjaksjhdkjashkjd---}}
+
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
