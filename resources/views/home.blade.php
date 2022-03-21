2ec19158dead0266923017639895f639c19912b7
Umar FarooQ <78466453+Umar-444@users.noreply.github.com>
2022-03-21 15:05:33 +0500

    change by me

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('frontend') }}</div>
-
+{{-- hello this is testing text --}}
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
