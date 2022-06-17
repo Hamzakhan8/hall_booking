43f031ecfd0047f5baabdec93d87ff8718fd1162
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-06-17 12:51:23 +0500

    Changes in seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,46 +30,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'role' => 'admin'
         ]);
-
-        /**
-         * Creates the relationship seeders data into
-         * the database
-         */
-        User::factory(3)->create()->each(function ($user)
-        {
-            $user->profile()->save(Profile::factory()->make());
-            $user->hallCategory()->save(HallCategory::factory()->make());
-            // $user->hall()->save(Hall::factory()->make());
-            // $user->bookings()->save(Bookings::factory()->make());
-            // $user->comments()->save(Comments::factory()->make());
-            $user->contact()->save(Contacts::factory()->make());
-            // $user->halls_meta()->save(Halls_meta::factory()->make());
-            // $user->reply()->save(Reply::factory()->make());
-            // $user->reviews()->save(Reviews::factory()->make());
-            $user->transactions()->save(Transactions::factory()->make());
-
-        });
-
-        // Hall::factory(3)->create()->each(function ($hall)
-        // {
-        //     $hall->bookings()->save(Bookings::factory()->make());
-        //     $hall->comments()->save(Comments::factory()->make());
-        //     $hall->reviews()->save(Reviews::factory()->make());
-        // });
-
-        // HallCategory::factory(3)->create()->each(function ($hallCategory)
-        // {
-        //     $hallCategory->hall()->save(Hall::factory()->make());
-        // });
-
-        // Reply::factory(3)->create()->each(function ($reply)
-        // {
-        //     $reply->comment()->save(Comments::factory()->make());
-        // });
-
-        // Bookings::factory(3)->create()->each(function ($bookings)
-        // {
-        //     $bookings->hall()->save(Hall::factory()->make());
-        // });
     }
 }
